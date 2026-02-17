ad1d1c841b1220939a9afcd0bb05501a1c353928
Ekk Store <ekkstore.id@gmail.com>
2026-02-17 17:02:14 +0700

    Change timeout duration in API handler
    Reduce timeout duration from 4 seconds to 2 seconds.

--- api/cek.js
+++ api/cek.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   const target = 'http://wyiiprivatajaib.srv-cloud.biz.id:2551';
   try {
     const controller = new AbortController();
-    const timeoutId = setTimeout(() => controller.abort(), 4000); // 4 detik timeout
+    const timeoutId = setTimeout(() => controller.abort(), 2000); // 4 detik timeout
 
     const response = await fetch(target, { method: 'GET', signal: controller.signal });
     clearTimeout(timeoutId);
