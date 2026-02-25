cad4f6c116bd2e211b51bcbe8ce648058ee6df05
Ekk Store <ekkstore.id@gmail.com>
2026-02-25 15:17:53 +0700

    Update target URL in cek.js to include '/Login'

--- api/cek.js
+++ api/cek.js
@@ -1,6 +1,6 @@
 // api/cek.js
 export default async function handler(req, res) {
-  const target = 'http://kyzzpublic.gantengdann.my.id:10542';
+  const target = 'http://kyzzpublic.gantengdann.my.id:10542/Login';
   try {
     const controller = new AbortController();
     const timeoutId = setTimeout(() => controller.abort(), 10000); // 0,5detik timeout
