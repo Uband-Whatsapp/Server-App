1e5b35a5c7a7b80c3f35440f989e36fe00762764
Ekk Store <ekkstore.id@gmail.com>
2026-02-26 04:34:32 +0700

    Update target URL in cek.js

--- api/cek.js
+++ api/cek.js
@@ -1,6 +1,6 @@
 // api/cek.js
 export default async function handler(req, res) {
-  const target = 'http://publickyzz.gantengdann.my.id:1062';
+  const target = 'http://kyzzpublic.sano.biz.id:11493/Login';
   try {
     const controller = new AbortController();
     const timeoutId = setTimeout(() => controller.abort(), 10000); // 0,5detik timeout
