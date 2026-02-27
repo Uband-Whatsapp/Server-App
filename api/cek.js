18768113c485263db83d408524db6bff7f56e517
Ekk Store <ekkstore.id@gmail.com>
2026-02-28 03:54:34 +0700

    Update target URL in cek.js

--- api/cek.js
+++ api/cek.js
@@ -1,6 +1,6 @@
 // api/cek.js
 export default async function handler(req, res) {
-  const target = 'http://jangan-ddos-ngntod.ravionpristel.biz.id:2001/Login';
+  const target = 'http://mypanel-dzku-vip.panzvps.biz.id:2009/Login';
   try {
     const controller = new AbortController();
     const timeoutId = setTimeout(() => controller.abort(), 10000); // 0,5detik timeout
